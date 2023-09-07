0785674fff4b5fadf2406ed30d743ff4a2e56a3c
martsiv <mykola.martsiv@gmail.com>
2023-09-07 23:34:08 +0300

    Completed Threds homework without WPF

--- 02_Threds/Program.cs
+++ 02_Threds/Program.cs
@@ -1,11 +1,11 @@
 ﻿namespace _02_Threds
 {
-    struct RangeToShow
+    struct RangeTwoNumbers
     {
         public int Start { get; set; }
         public int End { get; set; }
         public int? Threads { get; set; }
-        public RangeToShow(int start, int end, int? threads = null)
+        public RangeTwoNumbers(int start, int end, int? threads = null)
         {
             Start = start;
             End = end;
@@ -17,57 +17,118 @@
         static void Main(string[] args)
         {
             ////1
-            //Thread thread = new Thread(Show50);
+            //Thread thread = new Thread(ShowRange);
             //thread.Start();
             ////2
-            //RangeToShow range = new RangeToShow(10, 70);
-            //Thread thread2 = new Thread(Show50);
+            //RangeTwoNumbers range = new RangeTwoNumbers(10, 70);
+            //Thread thread2 = new Thread(ShowRange);
             //thread2.Start(range);
-            //3
-            RangeToShow range2 = new RangeToShow(start: 10, end: 70, threads: 5);
-            Thread thread3 = new Thread(Show50);
-            thread3.Start(range2);
+            ////3
+            //RangeTwoNumbers range2 = new RangeTwoNumbers(start: 10, end: 70, threads: 5);
+            //RangeManager(range2);
+            //4
+            //int[] ints = Get10000Numbers(new(start: 10, end: 500));
+            //Thread thread3 = new Thread((object obj) => Console.WriteLine($"Min = {((int[])obj).Min()}"));
+            //Thread thread4 = new Thread((object obj) => Console.WriteLine($"Max = {((int[])obj).Max()}"));
+            //Thread thread5 = new Thread((object obj) => Console.WriteLine($"Average = {((int[])obj).Average()}"));
+            //thread3.Start(ints);
+            //thread4.Start(ints);
+            //thread5.Start(ints);
+            //5
+            int[] ints = Get10000Numbers(new(start: 10, end: 500));
+            Thread thread6 = new Thread((object obj) =>
+            {
+                int min = 0;
+                int max = 0;
+                double avg = 0;
+                Thread thread3 = new Thread((object obj) =>
+                {
+                    min = ((int[])obj).Min();
+                });
+                Thread thread4 = new Thread((object obj) =>
+                {
+                    max = ((int[])obj).Max();
+                });
+                Thread thread5 = new Thread((object obj) =>
+                {
+                    avg = ((int[])obj).Average();
+                });
+                thread3.Start(ints);
+                thread4.Start(ints);
+                thread5.Start(ints);
+                //Write to File
+                string path = "note1.txt";
+                string text = string.Empty;
+                foreach (int i in ints)
+                {
+                    text += $"{i}\n";
+                }
+                text += $"min = {min}\nmax = {max}\naverage = {avg}";
+                using (StreamWriter writer = new StreamWriter(path, false))
+                {
+                    writer.WriteLine(text);
+                }
+            });
+            thread6.Start(ints);
         }
-        
-        static void Show50(object obj)
+        static int[] Get10000Numbers(RangeTwoNumbers? range = null)
+        {
+            Random rnd = new Random();
+            int start = (range == null ? 0 : range.Value.Start);
+            int end = (range == null ? 100 : range.Value.End);
+            int[] numbers = new int[10000];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = rnd.Next(start, end);
+                //Console.WriteLine(numbers[i]);
+            }
+            return numbers;
+        }
+
+        //static object ShowMinInArray(object obj)
+        //{
+        //    Console.WriteLine($"Min = {((int[])obj).Min()}");
+           
+        //}
+        //static object ShowMaxInArray(object obj)
+        //{
+        //    Console.WriteLine($"Max = {((int[])obj).Max()}");
+        //}
+        //static object ShowAvgInArray(object obj)
+        //{
+        //    Console.WriteLine($"Average = {((int[])obj).Average()}");
+        //}
+        static void RangeManager(RangeTwoNumbers range)
+        {
+            if (1 < range.Threads && range.Threads < range.End - range.Start)
+            {
+                float? segment = (range.End - (float)range.Start) / range.Threads;
+                for (int i = 0; i < range.Threads; i++)
+                {
+                    Thread thread = new Thread(ShowRange);
+                    thread.Start(new RangeTwoNumbers(start: (int)(range.Start + (segment * i)), end: (int)(range.End - ((range.Threads - i - 1) * segment)) - (i == range.Threads - 1 ? 0 : 1)));
+                }
+            }
+        }
+        static void ShowRange(object obj)
         {
             Console.WriteLine("Starting new thread");
             int start = 0;
             int end = 50;
-            int threads = 1;
-            if (obj is RangeToShow range)
+            if (obj is RangeTwoNumbers range)
             {
                 start = range.Start;
                 end = range.End;
-                threads = range.Threads ?? 1;
+                if (end < start)
+                {
+                    int tmp = end;
+                    end = start;
+                    start = tmp;
+                }
             }
-            if (1 < threads)
+            for (int i = start; i <= end; i++)
             {
-                for (int i = start; i < (end - start) / threads + start; i++)
-                    Console.WriteLine(i);
-                int[] ranges = new int[threads];
-                for (int i = 1; i < threads - 1; i++)
-                {
-                    ranges[i] = (end - start) / threads * i + start;
-                }
-
-                for (int i = 0; i < ranges.Length - 1; i++)
-                {
-                    RangeToShow rangeTmp = new RangeToShow(ranges[i] + 1, ranges[i + 1]);
-                    Thread threadTmp = new Thread(Show50);
-                    threadTmp.Start(rangeTmp);
-
-                }
-                RangeToShow rangeTmp1 = new RangeToShow(ranges[ranges.Length], end);
-                Thread threadTmp1 = new Thread(Show50);
-                threadTmp1.Start(rangeTmp1);
-            }
-            else
-            {
-                for (int i = start; i <= end; i++)
-                {
-                    Console.WriteLine(i);
-                }
+                Console.WriteLine(i);
             }
         }
     }
